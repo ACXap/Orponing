8b3c4257a895ba3d1590480cf4ccda34490f97c6
Aleksandr <mitkov1@inbox.ru>
2020-06-08 10:17:58 +0700

    Адрес добавлено получение значений одной строкой

--- OrponingTest/Program.cs
+++ OrponingTest/Program.cs
@@ -11,27 +11,7 @@ namespace OrponingTest
     {
         static void Main(string[] args)
         {
-            
 
-            // // Одиночный успешный запрос
-            // var a = new OrponingSoap(new FileRepository("inputSinglOk.txt"), new SerializeXml());
-
-            // var result = a.GetOrponByAddress("test");
-
-            // // Одиночный с ошибками запрос
-            //// a = new OrponingSoap(new FileRepository("inputSinglError.txt"), new SerializeXml());
-
-            // //result = a.GetOrponByAddress("test");
-
-            // // Групповой успешный запрос
-            //  a = new OrponingSoap(new FileRepository("inputGroopOk.txt"), new SerializeXml());
-
-            // result = a.GetOrponByAddress("test");
-
-            // // Групповой с ошибками запрос
-            //// a = new OrponingSoap(new FileRepository("inputGroopError.txt"), new SerializeXml());
-
-            //// result = a.GetOrponByAddress("test");
         }
     }
 }
